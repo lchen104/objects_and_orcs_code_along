679b6812ce9ca36bad0ce2849a589d61382082b4
lchen104 <lchen@zdmedia.com>
2023-06-14 11:23:43 -0400

    created an object

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+// using array inside an object
+
+
+// create an object
+const adventurer = {
+    name: "Leo",
+    hitPoint: 10,
+    belongings: ["staff", "potions", "medicine" ]
+}
+
+// access the values in the array
+
+console.log(adventurer.belongings[0]);
