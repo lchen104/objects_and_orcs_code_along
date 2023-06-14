d9ecb77a35c902c7d24def621d8778d44254f60e
lchen104 <lchen@zdmedia.com>
2023-06-14 11:35:15 -0400

    created an object within an object

--- app.js
+++ app.js
@@ -5,9 +5,29 @@
 const adventurer = {
     name: "Leo",
     hitPoint: 10,
-    belongings: ["staff", "potions", "medicine" ]
+    belongings: ["staff", "potions", "medicine" ],
+    companion: {
+        name: "stinky",
+        type: "cat"
+    }
 }
 
-// access the values in the array
+// access the values in the array of an array
+console.log(`My companions name is ${adventurer.companion}`);
 
+// access the values in the array
+console.log(adventurer.belongings);
+
+// access the first belonging
 console.log(adventurer.belongings[0]);
+
+
+// access all the belongings
+//itterate over an array inside of an object
+for (let i = 0; i < adventurer.belongings.length; i++) {
+    console.log(adventurer.belongings[i]);
+}
+
+
+// #1 OBJ within OBJ
+
